fc5c83d18e8bb61ab0998bbd3b0df4342a9de8dc
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2019-10-31 13:26:30 +0100

    up du menu
    meilleur menu plus beau avec bouton a l'horizontal

--- testsouris.py
+++ testsouris.py
@@ -15,6 +15,16 @@ CIEL = 0, 200, 255
 GREEN = 0, 255, 0
 YELLOW = 255, 255, 0
 BLACK = 0,0,0
+
+xBoutonStart = 50
+yBoutonStart = 10
+
+xBoutonReprendre = 250
+yBoutonReprendre = 10
+
+xBoutonQuitter = 450
+yBoutonQuitter = 10
+
 def main():
     
     fenetre = pygame.display.set_mode((640, 300))
@@ -22,7 +32,9 @@ def main():
     start = pygame.image.load("start.png").convert()
     reprendre = pygame.image.load("reprendre.png").convert()
     quitter = pygame.image.load("quitter.png").convert()
-
+    imgmenutest = pygame.image.load("imgmenutest.png").convert()
+    
+    
 
     loop = True
     
@@ -36,22 +48,20 @@ def main():
             niv = Niveau(difficulté,mode_affichage)
             NIVEXIST = True
 
-        background = pygame.Surface(fenetre.get_size())
-        background.fill(CIEL)
 
         # Ajout du fond dans la fenêtre
-        fenetre.blit(background, (0, 0))
+        fenetre.blit(imgmenutest, (0, 0))
 
         # ça crée des rectangles qui permettent de zoner les zones clickables...
-        rect_green = pygame.draw.rect(fenetre, green_color, [250, 10, 100, 50])
+        rect_green = pygame.draw.rect(fenetre, green_color, [xBoutonStart, yBoutonStart, 100, 50])
         #...  puis sont remplacées par les boutons
-        fenetre.blit(start, (250,10))
+        fenetre.blit(start, (xBoutonStart,yBoutonStart))
 
-        rect_yellow = pygame.draw.rect(fenetre, yellow_color, [250, 85, 100, 50])
-        fenetre.blit(reprendre, (250,85))
+        rect_yellow = pygame.draw.rect(fenetre, yellow_color, [xBoutonReprendre, yBoutonReprendre, 100, 50])
+        fenetre.blit(reprendre, (xBoutonReprendre,yBoutonReprendre))
 
-        rect_black= pygame.draw.rect(fenetre, black_color, [250, 160, 100, 50])
-        fenetre.blit(quitter, (250,160))
+        rect_black= pygame.draw.rect(fenetre, black_color, [xBoutonQuitter, yBoutonQuitter, 100, 50])
+        fenetre.blit(quitter, (xBoutonQuitter,yBoutonQuitter))
 
         # retourne 1 si le curseur est au dessus du bouton
         mouse_xy = pygame.mouse.get_pos()
